39c2a65128c2a351a4f2789ad53612b51a7dd2bf
ShushmaPriyaVadlamanu <149227381+ShushmaPriyaVadlamanu@users.noreply.github.com>
2024-06-26 11:05:10 +0530

    Output.py

---
+++ Output.py
@@ -0,0 +1,12 @@
+C:\Users\Sushma\PycharmProjects\PRODIGY_CS_01\.venv\Scripts\python.exe C:\Users\Sushma\PycharmProjects\PRODIGY_CS_01\Caser_Cipher.py 
+Type 'encrypt' to encrypt, 'decrypt' to decrypt, or 'exit' to quit: encrypt
+Enter your message: shushma priya
+Enter shift value: 2
+Encrypted message: ujwujoc rtkac
+Type 'encrypt' to encrypt, 'decrypt' to decrypt, or 'exit' to quit: decrypt
+Enter your message: ujwujoc rtkac
+Enter shift value: 2
+Decrypted message: shushma priya
+Type 'encrypt' to encrypt, 'decrypt' to decrypt, or 'exit' to quit: exit
+
+Process finished with exit code 0
